988b3b239bc9a352f22d35c20abf5d44750de8bd
azama <a.kurmanov@csi.kz>
2022-10-31 21:35:26 +0600

    Initial commit

---
+++ src/main/java/ru/gb/javaspringlessons/JavaSpringLessonsApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.javaspringlessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLessonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringLessonsApplication.class, args);
+    }
+
+}
